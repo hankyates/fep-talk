b3638e04295c259cc87caecd4287d7237d31d2cc
Hank Yates <hankyates@gmail.com>
2018-08-29 22:22:29 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+ws$ = rxjs.webSocket.webSocket('wss://ws-feed.pro.coinbase.com')
+ws$.next({
+  "type": "subscribe",
+  "product_ids": [
+    "ETH-USD",
+  ],
+  "channels": [
+    "heartbeat",
+    {
+      "name": "ticker",
+      "product_ids": [
+        "ETH-USD",
+      ]
+    }
+  ]
+})
+
